a0bef942dde7d94e8a5c47a842a372311add4e3f
Emmanuel <emmanueltar14@gmail.com>
2022-03-04 15:22:17 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame frame = new JFrame();
+        frame.setSize(100,100);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setVisible(true);
+        JPanel panel =  new JPanel();
     }
 }
